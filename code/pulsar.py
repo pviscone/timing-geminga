e84d6419b152b7a3eaccfb45b638628e67deab7b
pviscone <pviscone@localhost.localdomain>
2021-04-29 04:43:43 +0200

    Fortran subroutine replaced with numba jit compiler. Light curves, scatter phase-time and ztest heat map added

--- code/pulsar.py
+++ code/pulsar.py
@@ -4,8 +4,8 @@ from astropy.table import Table
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-import ztest
-import importlib as il
+#import ztest
+from numba import jit
 
 src_dir = os.getcwd()
 main_dir = os.path.dirname(src_dir)
@@ -17,136 +17,215 @@ f1_start=-1.95250e-13
 t0= 250992001
 
 class Data():
-    def __init__(self,fit_name):
+    def __init__(self,fit_name,ztest_load=False):
         self.fit_file=fits.open(os.path.join(data_dir,fit_name))
         self.data=Table(self.fit_file[1].data)
         self.gti=Table(self.fit_file[2].data)
         self.data_header=self.fit_file[1].header
         self.gti_header=self.fit_file[2].header
-        self.data.add_column(0,name="phase")
+        self.ph_time=np.array(self.data["TIME"])-t0
+        if ztest_load==True:      
+            self.load()
+        """ 
+        self.z_test
+        self.z_test_max
+        self.f0_best
+        self.f1_best
+        self.f0_arr
+        self.f1_arr
+        self.phase 
+        """
         
     def __repr__(self):
         return str(self.data)
     
-    def mapping(self,energy,vmax=150):
+    def __len__(self):
+        return len(self.data)
+    
+    def mapping(self,energy,vmax=120):
         gamma_E=self.data[self.data["ENERGY"]>energy]
-        plt.figure(figsize=(6,10))
-        plt.subplot(211)
-        plt.hexbin(gamma_E["L"],gamma_E["B"],cmap='plasma',vmax=vmax)
+        plt.figure(figsize=(7,6))
+        plt.subplot(111)
+        xm=195.1317
+        d=2
+        x0=xm-d
+        x1=xm+d
+        ym=4.2252836
+        y0=ym-d
+        y1=ym+d
+        plt.hist2d(gamma_E["L"],gamma_E["B"],cmap='plasma',vmax=vmax,bins=(80,80),range=[[x0,x1],[y0,y1]])
         plt.colorbar(label='counts')
         plt.xlabel("L [°]")
         plt.ylabel("B [°]")
-        plt.title(f"Gamma with Energy>{energy} map")
+        #plt.xlim(194.1317,196.1317)
+        #plt.ylim(3.4252836,5.0252836)
+        plt.title(f"Gamma with Energy>{energy} MeV map")
+        plt.show()
         
-        plt.subplot(212,projection='aitoff')
-        l=np.array([x if x<180 else x-360 for x in gamma_E[gamma_E["L"]>180]["L"]])*np.pi/180
-        plt.plot(l[0],gamma_E["B"][0]*np.pi/180,".",markersize=20)
-        #plt.xlim(-180,180)
-        #plt.ylim(-90,90)
+    def aitoff(self):
+        plt.subplot(111,projection='aitoff')
+        l=np.array([x if x<180 else x-360 for x in self.data[self.data["L"]>180]["L"]])*np.pi/180
+        plt.plot(l[10],self.data["B"][10]*np.pi/180,".",markersize=20)
         plt.grid()
         plt.show()
+        
+    def energy_plot(self):
+        plt.figure()
+        #hist=plt.hist(self.data["ENERGY"],bins=200,range=(0,8000))
+        hist=plt.hist(self.data["ENERGY"],bins=200,range=(0,8000))
+        def exp(x,a,t):
+            return a*np.exp(-x/t)
+        popt,pcov=curve_fit(exp,hist[1][3:-1],hist[0][3:],p0=[2000,1000])
+        x=np.linspace(60,8000,400)
+        plt.plot(x,exp(x,*popt))
+        #plt.plot(hist[1][3:-1],hist[0][3:],".")
+        plt.show()
+        return popt
     
+        
+    
+    def load(self):
+        self.z_test=np.load(os.path.join(data_dir,"z_test.npy"))
+        self.f0_arr=np.load(os.path.join(data_dir,"f0_arr.npy"))
+        self.f1_arr=np.load(os.path.join(data_dir,"f1_arr.npy"))
+        self.phase=np.load(os.path.join(data_dir,"phase.npy"))
+        self.z_test_max=np.max(self.z_test)
+        f_best_index=np.unravel_index(np.argmax(self.z_test),self.z_test.shape)
+        self.f0_best=self.f0_arr[f_best_index[1]]
+        self.f1_best=self.f1_arr[f_best_index[0]]
+        
+        
+    def save(self):
+    
+        np.save(os.path.join(data_dir,"z_test.npy"),self.z_test)
+        np.save(os.path.join(data_dir,"f0_arr.npy"),self.f0_arr)
+        np.save(os.path.join(data_dir,"f1_arr.npy"),self.f1_arr)
+        np.save(os.path.join(data_dir,"phase.npy"),self.phase)
 
+    ###!DEPRECATED ZTEST WITH FORTRAN: GIVE ARITHMETIC ERRORS THAT MESS EVERYTHING
+    """       
+    def ztest(self, save=False,
+                    f0_start=4.21786,
+                    delta0=  0.00004,
+                    n0=500,
+                    #f1_start=-1.9525000000000000e-13,
+                    f1_start=-1.951e-13,
+                    delta1=5.00000e-16,
+                    n1=100,
+                    n_harm=10):   
+
+            
+
+        self.f0_arr=np.linspace(f0_start-delta0,f0_start+delta0,n0)
+        self.f1_arr=np.linspace(f1_start-delta1,f1_start+delta1,n1)
+        z_test=ztest.mod.matrix(self.ph_time,self.f0_arr,self.f1_arr,len(self.data),n_harm,len(self.f0_arr),len(self.f1_arr))
+        self. z_test=(2/len(self.data))*z_test
+        self.z_test_max=np.max(z_test)
+        arg=np.argmax(z_test)
+        f_best_index=np.unravel_index(arg,z_test.shape)
+        self.f0_best=self.f0_arr[f_best_index[0]]
+        self.f1_best=self.f1_arr[f_best_index[1]]
+        self.phase=((self.f0_best*self.ph_time+(self.f1_best*np.power(self.ph_time,2))/2)%1)*2*np.pi
+        
+        if save==True:
+            self.save()            
+        
+        
+        return [self.f0_best,self.f1_best,self.z_test_max]
+    """
+    @jit
+    def phase_func(self,f0,f1):
+        return ((f0*self.ph_time+(f1*np.power(self.ph_time,2))/2)%1)*2*np.pi
+
+    
+    @jit   
+    def ztest_func(self,phase,n_harm):
+        k=np.arange(1,n_harm+1)
+        k=np.reshape(k,(k.shape[0],1))
+        return (2/len(self.data))*np.sum(np.power(np.sum(np.cos(phase*k),axis=1),2)+np.power(np.sum(np.sin(phase*k),axis=1),2))
+
+        
+    @jit    
+    def ztest(self, save=False,
+                    f0_start=4.21752323401995,
+                    df0=     0.00000005,
+                    n0=1000,                    
+                    f1_start=-1.96678571365e-13,
+                    df1=      2.0000e-14,
+                    n1=100,
+                    n_harm=10):
+        self.f0_arr=np.linspace(f0_start-df0,f0_start+df0,n0)
+        self.f1_arr=np.linspace(f1_start-df1,f1_start+df1,n1)
+        self.z_test=np.array([[self.ztest_func(self.phase_func(f0,f1),n_harm) for f0 in self.f0_arr] for f1 in self.f1_arr])
+        self.z_test_max=np.max(self.z_test)
+        arg=np.argmax(self.z_test)
+        f_best_index=np.unravel_index(arg,self.z_test.shape)
+        self.f0_best=self.f0_arr[f_best_index[1]]
+        self.f1_best=self.f1_arr[f_best_index[0]]
+        self.phase=self.phase_func(self.f0_best,self.f1_best)
+        if save==True:
+            save()
+        
+    
+    """  def wrapp(self,f0_arr,f1_arr):
+        ##! TENTATIVO DI EVITARE LIST COMPREHENSION USANDO ARRAY 3D: ENORMI RISCHI DI SBAGLIARE L' INDICE DELL' ASSE SU CUI SI SOMMA
+        self.z_test=self.ztest(((self.phase(f0,f1) for f0 in f0_arr) for f1 in f1_arr))
+        return
+        """
+    
+    def ztest_map(self):
+        plt.figure(figsize=(11,10))
+        
+        #?ATTENZIONE AGLI ASSI FORSE SBAGLIATI (ora dovrebbero essere giusti)
+        
+        plt.imshow(self.z_test,aspect='auto',extent=[self.f0_arr[0],self.f0_arr[-1],self.f1_arr[-1],self.f1_arr[0]])
+        #plt.imshow(self.z_test,aspect='auto')
+        plt.colorbar()
+        plt.xlabel("$f_0$ [Hz]")
+        plt.ylabel("$f_1$ [Hz]")
+        plt.title("Z Test($f_0$ , $f_1$)")
+        h, w = (self.z_test).shape
+        plt.figure(figsize=(16, 8))
+        ax = plt.axes(projection='3d')
+        X, Y = np.meshgrid(np.arange(w), np.arange(h))
+        ax.plot_surface(X, Y, self.z_test, rstride=1, cstride=1, cmap='viridis', edgecolor='none', antialiased=False)
+                
+        plt.show()
+
+    def scatter_phase(self):
+        plt.plot(self.phase,self.ph_time,".",markersize=2)
+        plt.figure()
+        plt.hist2d(self.phase,self.ph_time,bins=(100,100))
+        plt.show()
+        
+    def light_curve(self,e_min=0,e_max=500000,bins=100,offset=0):
+        ph=self.phase[(self.data["ENERGY"]>e_min) & (self.data["ENERGY"]<e_max)]
+        ph=((ph+offset)%(2*np.pi))
+        plt.hist(ph/np.pi,bins)
+        plt.show()
+        
 
 
 tt=Data("PSRJ_Geminga_3deg_100mev_tt.fits")
-bary=Data("PSRJ_Geminga_3deg_100mev_bary.fits")
+bary=Data("PSRJ_Geminga_3deg_100mev_bary.fits",ztest_load=True)
+
+
 
 
 tt.mapping(300)
 tt.mapping(1000)
+tt.aitoff()
+popt=tt.energy_plot()
 
 
 
-plt.figure()
-hist=plt.hist(tt.data["ENERGY"],bins=200,range=(0,8000))
-plt.figure()
-def exp(x,a,t):
-    return a*np.exp(-x/t)
-popt,pcov=curve_fit(exp,hist[1][3:-1],hist[0][3:],p0=[2000,1000])
-x=np.linspace(60,8000,400)
-plt.plot(x,exp(x,*popt))
-plt.plot(hist[1][3:-1],hist[0][3:],".")
+#bary.ztest()
+bary.ztest_map()
+
+bary.scatter_phase()
+bary.light_curve(offset=1)
 
 
-
-#il.reload(ztest)
-n0=500
-delta0=0.2
-
-n1=50
-delta1=5e-15
-""" 
-f0_start=2.4179008016032064
-f1_start=4.374030612244898e-13
-"""
-
-"""
-f0_start=2.7595841683366733
-f1_start=4.337295918367347e-13
-"""
-
-f0_start=2.856978957915832
-f1_start=4.2872959183673473e-13
-
-f0_arr=np.linspace(f0_start-delta0,f0_start+delta0,n0)
-f1_arr=np.linspace(f1_start-delta1,f1_start+delta1,n1)
-
-ph_time=np.array(bary.data["TIME"])-t0
-np.save("ph_time.npy",ph_time)
-
-
-z_test=ztest.mod.matrix(ph_time,f0_arr,f1_arr)
-z_test=(2/len(bary.data))*z_test
-#np.save("z_test.npy",z_test)
-
-plt.figure(figsize=(10,10))
-
-#ATTENZIONE AGLI ASSI FORSE SBAGLIATI
-plt.imshow(z_test,aspect='auto',extent=[f1_arr[0],f1_arr[-1],f0_arr[-1],f0_arr[0]])
-plt.colorbar()
-plt.xlabel("$f_1$ [Hz]")
-plt.ylabel("$f_0$ [Hz]")
-plt.title("Z Test($f_0$ , $f_1$)")
-
-z_test_max=np.max(z_test)
-arg=np.argmax(z_test)
-f_best_index=np.unravel_index(arg,z_test.shape)
-f0_best=f0_arr[f_best_index[0]]
-f1_best=f1_arr[f_best_index[1]]
-
-
-
-
-
-
-
-
-
-""" #f0 scorre sulle righe ->, f1 scorre sulle colonne |
-ffdot=np.array([[(x,y) for x in f0_arr] for y in f1_arr])
-
-def phi(t,f0,f1):
-    return (f0*(t-t0)+(f1*(t-t0)**2)/2)%1
-
-def z(k,phi_arr):
-    return np.sum(np.cos(k*phi_arr*2*np.pi)**2)+np.sum(np.sin(k*phi_arr*2*np.pi)**2)
-
-def ztest(phi_arr,n=10,N=len(bary.data)):
-    return (2/N)*np.sum((z(k,phi_arr) for k in range(1,n+1)))
-
-
-#ztest=[[[phi(t,f0,f1) for f0 in f0_arr]for f1 in f1_arr] for t in bary.data["TIME"]]
-
-
-z_mat=[[ztest(np.array([phi(t,x,y) for t in bary.data["TIME"]])) for x in f0_arr] for y in f1_arr]
-
-
-"""
-
-
-
-
-""" 
-np.save("phase.npy",phase)
-#phase=np.load("phase.npy",allow_pickle=True) """
+#TODO Perchè le curve luce vengono così diverse dal paper?
+#TODO Perchè nello scatter fase,termpo le righe di accumulazione sono interrotte a circa metà?
+#TODO Fare fit 3D con doppia gaussiana sullo ztest e assegnare errori a f0 e f1
--- code/test.f90
+++ code/test.f90
@@ -1,37 +1,39 @@
 module mod
     implicit none 
     contains   
-    subroutine matrix(ph_time,f0_arr,f1_arr,result)
+    subroutine matrix(ph_time,f0_arr,f1_arr,result,N,n_harm,size_f0,size_f1)
     ! ph_time= array tempi fotoni, N=# fotoni, n_harm=#armoniche, f0_arr=array_f0, size_f0=len(f0_arr), f1_arr=array_f1, size_f1=len(f1_arr), result=matrice degli z test OUTPUT
-        real*8, dimension(30957), intent(in) :: ph_time
-        real*8, dimension(500), intent(in) :: f0_arr
-        real*8, dimension(50), intent(in) :: f1_arr
-        real*8, dimension(500,50), intent(out)  :: result
+        integer, intent(in)::N, n_harm, size_f0,size_f1
+
+        real*8, dimension(N), intent(in) :: ph_time
+        real*8, dimension(size_f0), intent(in) :: f0_arr
+        real*8, dimension(size_f1), intent(in) :: f1_arr
+        real*8, dimension(size_f0,size_f1), intent(out)  :: result
         !real, dimension(30957) :: phi_arr
         real*8, dimension(:), allocatable :: phi_arr       
         !integer*8 :: N, n_harm, size_f0, size_f1, i, j, k, p
         integer*8 :: i,j,k,p 
         !real*16 ::  t0, cs, sn, ztest
         real*8 :: ztest, cs, sn
-        allocate(phi_arr(30957))
+        allocate(phi_arr(N))
         !N=30957
         !n_harm=10
         !size_f0=50
         !size_f1=50        
         !t0=250992001.      
         
-        do i=1, 500
-            do j=1, 50
+        do i=1, size_f0
+            do j=1, size_f1
                 ztest=0.
                 phi_arr=modulo((f0_arr(i)*(ph_time)+(f1_arr(j)*(ph_time)**2)/2),1.)
                 
                 phi_arr=phi_arr*2*3.1415927
                 
-                do k=1, 10
+                do k=1, n_harm
                     cs=0.
                     sn=0.  
 
-                    do p=1, 30957             
+                    do p=1, N             
 
                         cs=cs+cos(k*phi_arr(p))
                         sn=sn+sin(k*phi_arr(p))
